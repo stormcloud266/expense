8224a0f8738eeffc356020fdce6fb8daf079fe7e
Tawnee Allport <allprt89@gmail.com>
2020-11-02 18:53:46 -0700

    add router and links

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route, Link, NavLink } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+const Dashboard = () => (
+  <div>Dashboard</div>
+)
+const EditExpense = () => (
+  <div>EditExpense</div>
+)
+const CreateExpense = () => (
+  <div>CreateExpense</div>
+)
+const Help = () => (
+  <div>Help</div>
+)
+const NotFound = () => (
+  <div>NotFound - <Link to="/">Go Home</Link></div>
+)
+
+const Header = () => (
+  <header>
+    <h1>Expenses</h1>
+    <nav>
+      <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+      <NavLink to="/edit" activeClassName="is-active" exact={true}>Edit</NavLink>
+      <NavLink to="/create" activeClassName="is-active" exact={true}>Create</NavLink>
+      <NavLink to="/help" activeClassName="is-active" exact={true}>Help</NavLink>
+    </nav>
+  </header>
+)
+
+const routes = (
+  <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route path="/" exact={true} component={Dashboard} />
+      <Route path="/edit" exact={true} component={EditExpense} />
+      <Route path="/create" exact={true} component={CreateExpense} />
+      <Route path="/help" exact={true} component={Help} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {routes}
   </React.StrictMode>,
   document.getElementById('root')
 );
