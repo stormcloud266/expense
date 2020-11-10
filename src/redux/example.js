39d3963029e8f34891af8ec0d311ab55f44b89a3
Tawnee Allport <allprt89@gmail.com>
2020-11-10 11:54:33 -0700

    work on adding redux

---
+++ src/redux/example.js
@@ -0,0 +1,66 @@
+import { createStore, combineReducers } from 'redux'
+import uuid from 'uuid'
+
+// Action generators 
+const removeExpense = ({ id } = {}) => ({
+  type: "REMOVE_EXPENSE",
+  id
+})
+
+const addExpense = (
+  { 
+    description = '', 
+    note = '', 
+    amount = 0, 
+    createdAt = 0 
+  } = {}
+) => ({
+  type: "ADD_EXPENSE",
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+})
+
+// Expenses reducer
+const expensesReducerDefaultState = []
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    case "ADD_EXPENSE":
+      return [
+        ...state,
+        action.expense
+      ]
+    case "REMOVE_EXPENSE":
+        return state.filter(expense => expense.id !== action.id)
+    default:
+      return state
+  }
+}
+
+// Filters reducer
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined,
+}
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+// Store
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+)
