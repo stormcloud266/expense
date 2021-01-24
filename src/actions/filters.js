6eb992a5eed8db51640b5f491de6893a67e79507
Tawnee Allport <allprt89@gmail.com>
2021-01-23 19:02:25 -0700

    add start and end dates. fix filter action spelling error

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,22 +1,22 @@
 export const setTextFilters = (text = '') => ({
-  type: "SET_TEXT_FILTER",
-  text
+	type: 'SET_TEXT_FILTER',
+	text,
 })
 
 export const sortByAmount = () => ({
-  type: "SORT_BY_AMOUNT"
+	type: 'SORT_BY_AMOUNT',
 })
 
 export const sortByDate = () => ({
-  type: "SORT_BY_DATE"
+	type: 'SORT_BY_DATE',
 })
 
 export const setStartDate = (date) => ({
-  type: "SET_START_DATA",
-  date
+	type: 'SET_START_DATE',
+	date,
 })
 
 export const setEndDate = (date) => ({
-  type: "SET_END_DATA",
-  date
+	type: 'SET_END_DATE',
+	date,
 })
--- src/components/ExpenseListFilters/ExpenseListFilters.js
+++ src/components/ExpenseListFilters/ExpenseListFilters.js
@@ -1,32 +1,61 @@
-import React from "react"
-import { connect } from "react-redux"
-import { sortByAmount, sortByDate, setTextFilters } from "../../actions/filters"
-import styles from "./styles.module.css"
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { DateRangePicker } from 'react-dates'
+import {
+	sortByAmount,
+	sortByDate,
+	setTextFilters,
+	setStartDate,
+	setEndDate,
+} from '../../actions/filters'
+import styles from './styles.module.css'
 
-const ExpenseListFilters = ({ filters, dispatch }) => (
-	<div>
-		<input
-			type="text"
-			value={filters.text}
-			onChange={(e) => {
-				dispatch(setTextFilters(e.target.value))
-			}}
-		/>
-		<select
-			value={filters.sortBy}
-			onChange={(e) => {
-				if (e.target.value === "amount") {
-					dispatch(sortByAmount())
-				} else if (e.target.value === "date") {
-					dispatch(sortByDate())
-				}
-			}}
-		>
-			<option value="date">Date</option>
-			<option value="amount">Amount</option>
-		</select>
-	</div>
-)
+const ExpenseListFilters = ({ filters, dispatch }) => {
+	const [calendarFocused, setCalendarFocused] = useState(null)
+
+	const onDatesChange = ({ startDate, endDate }) => {
+		dispatch(setStartDate(startDate))
+		dispatch(setEndDate(endDate))
+	}
+
+	return (
+		<div>
+			<input
+				type='text'
+				value={filters.text}
+				onChange={(e) => {
+					dispatch(setTextFilters(e.target.value))
+				}}
+			/>
+			<select
+				value={filters.sortBy}
+				onChange={(e) => {
+					if (e.target.value === 'amount') {
+						dispatch(sortByAmount())
+					} else if (e.target.value === 'date') {
+						dispatch(sortByDate())
+					}
+				}}
+			>
+				<option value='date'>Date</option>
+				<option value='amount'>Amount</option>
+			</select>
+
+			<DateRangePicker
+				startDate={filters.startDate}
+				endDate={filters.endDate}
+				startDateId='your_unique_start_date_id'
+				endDateId='your_unique_end_date_id'
+				onDatesChange={onDatesChange}
+				focusedInput={calendarFocused}
+				onFocusChange={(focusedInput) => setCalendarFocused(focusedInput)}
+				numberOfMonths={1}
+				isOutsideRange={() => false}
+				showClearDates={true}
+			/>
+		</div>
+	)
+}
 
 const mapStateToProps = (state) => ({
 	filters: state.filters,
