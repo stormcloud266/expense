46298780325974e679ca103cbefafcd8862dbf8f
Tawnee <allprt89@gmail.com>
2021-02-16 12:16:55 -0700

    add private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,24 +2,22 @@ import React from 'react'
 import { Router, Switch, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 
-import Header from '../components/Header/Header'
 import Login from '../components/Login/Login'
 import Dashboard from '../components/Dashboard/Dashboard'
 import EditExpense from '../components/EditExpense/EditExpense'
 import AddExpense from '../components/AddExpense/AddExpense'
 import Help from '../components/Help/Help'
 import NotFound from '../components/NotFound/NotFound'
-
+import PrivateRoute from './PrivateRoute'
 export const history = createBrowserHistory()
 
 const AppRouter = () => (
 	<Router history={history}>
-		<Header />
 		<Switch>
 			<Route path='/' exact={true} component={Login} />
-			<Route path='/dashboard' exact={true} component={Dashboard} />
-			<Route path='/edit/:id' exact={true} component={EditExpense} />
-			<Route path='/create' exact={true} component={AddExpense} />
+			<PrivateRoute path='/dashboard' exact={true} component={Dashboard} />
+			<PrivateRoute path='/edit/:id' exact={true} component={EditExpense} />
+			<PrivateRoute path='/create' exact={true} component={AddExpense} />
 			<Route path='/help' exact={true} component={Help} />
 			<Route component={NotFound} />
 		</Switch>
